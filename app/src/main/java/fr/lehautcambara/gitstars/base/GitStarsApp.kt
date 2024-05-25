07237cd5d933fce40250667344bca65e46105238
Charles Neveu <charles@cmorq.com>
2024-05-25 22:57:02 +0200

    Remove temporary dev stuff

--- app/src/main/java/fr/lehautcambara/gitstars/base/GitStarsApp.kt
+++ app/src/main/java/fr/lehautcambara/gitstars/base/GitStarsApp.kt
@@ -1,28 +1,18 @@
 package fr.lehautcambara.gitstars.base
 
 import android.app.Application
-import fr.lehautcambara.gitstars.bus.Bus
-import fr.lehautcambara.gitstars.bus.events.RepoRequestEvent
-import fr.lehautcambara.gitstars.bus.events.RepoResponseEvent
+import fr.lehautcambara.gitstars.bus.modules.DelayModule
 import fr.lehautcambara.gitstars.network.retrofit.GitServiceAPI
-import org.greenrobot.eventbus.Subscribe
-import org.greenrobot.eventbus.ThreadMode
 
 class GitStarsApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Bus.register(this)
         registerBusModules()
-        RepoRequestEvent().post()
     }
     private fun registerBusModules() {
+        DelayModule()
         GitServiceAPI()
     }
 
-    @Subscribe(threadMode = ThreadMode.MAIN_ORDERED)
-    fun onEvent(event: RepoResponseEvent) {
-        val response = event.response
-        val success = response.isSuccessful
-    }
 }
