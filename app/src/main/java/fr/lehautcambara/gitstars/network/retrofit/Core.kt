4f9543b96cf75745dddae23271078959a814f19d
Charles Neveu <charles@cmorq.com>
2024-05-25 22:46:29 +0200

    Increase timeout to 60 seconds because 10 secs was timing out.

--- app/src/main/java/fr/lehautcambara/gitstars/network/retrofit/Core.kt
+++ app/src/main/java/fr/lehautcambara/gitstars/network/retrofit/Core.kt
@@ -2,8 +2,10 @@ package fr.lehautcambara.gitstars.network.retrofit
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 object Core {
 
@@ -11,9 +13,15 @@ object Core {
         .addLast(KotlinJsonAdapterFactory())
         .build()
 
+    private val client: OkHttpClient = OkHttpClient().newBuilder()
+        .connectTimeout(60, TimeUnit.SECONDS)
+        .readTimeout(60, TimeUnit.SECONDS)
+        .build()
+
     fun retrofit(baseUrl: String = "https://api.github.com/") : Retrofit {
         return Retrofit.Builder()
             .baseUrl(baseUrl)
+            .client(client)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
     }
