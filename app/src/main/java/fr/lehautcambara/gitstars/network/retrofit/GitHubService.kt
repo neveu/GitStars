1b4e0ac1400369c9d72b318002c6fdfa22c136b9
Charles Neveu <charles@cmorq.com>
2024-05-29 23:41:41 +0200

    updated PAT again

--- app/src/main/java/fr/lehautcambara/gitstars/network/retrofit/GitHubService.kt
+++ app/src/main/java/fr/lehautcambara/gitstars/network/retrofit/GitHubService.kt
@@ -6,14 +6,16 @@ import retrofit2.http.Headers
 import retrofit2.http.Path
 
 private const val Authorization0 = "Authorization: Bearer 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"
-private const val Authorization = "Authorization: Bearer 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"
+private const val Authorization1 = "Authorization: Bearer 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"
+private const val Authorization2 = "Authorization: Bearer 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"
+
 
 interface GitHubService {
-    @Headers(Authorization)
+    @Headers(Authorization2)
     @GET("search/repositories?q=stars:>0&sort=stars&per_page=100")
     fun reposByStars() : Call<RepoList>
 
-    @Headers(Authorization)
+    @Headers(Authorization2)
     @GET("repos/{name}/{owner}/contributors")
     fun repoContributors(
         @Path("name") name: String,
